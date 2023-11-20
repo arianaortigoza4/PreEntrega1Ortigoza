2470bdaff55fca9424fe62120b4da3bd66579a98
arianaortigoza4 <arianaortigoza4@gmail.com>
2023-11-20 13:18:45 -0300

    estilos

--- src/componentes/ContacForm/ContacForm.jsx
+++ src/componentes/ContacForm/ContacForm.jsx
@@ -17,7 +17,6 @@ const ContacForm= ({onCreate}) =>{
         <Form.Label>Email</Form.Label>
         <Form.Control type="email" placeholder="Email" />
         <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
         </Form.Text>
       </Form.Group>
 
